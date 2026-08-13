135658e6e679801c1f6934838d39086c3ffb01b2
onyolumaxwell36-cpu <onyolumaxwell36@gmail.com>
2026-08-13 10:58:52 +0100

    MainActivity.java

--- app/src/main/java/com/atlas/wake/MainActivity.java
+++ app/src/main/java/com/atlas/wake/MainActivity.java
@@ -1,1 +1,23 @@
+package com.atlas.wake;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText(
+            "ATLAS Wake\n\n" +
+            "Wake-word system is ready."
+        );
+        text.setTextSize(22);
+        text.setPadding(40, 100, 40, 40);
+
+        setContentView(text);
+    }
+}
